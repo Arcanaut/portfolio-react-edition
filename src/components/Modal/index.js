337475738d7f9ca1ef8116d82a2a8ab326f4f6bb
Brendan Ahearn <91020994+Arcanaut@users.noreply.github.com>
2022-03-08 20:49:42 -0600

    Update index.js

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,35 +1,28 @@
 import React from 'react';
-import Photolist from 'Photolist';
+import Photolist from '../Photolist';
 
 function Modal(props) {
 
     return (
-      <div className="modalBackdrop">
-
-  <div className="modalContainer ui modal">
-    <div className="ui header">
-    <h3 className="modalTitle">{Photos}</h3>
+      <div class="ui basic modal">
+      <div class="ui icon header">
+        {/* <h3 class="modalTitle"> {photos} </h3> */}
+      </div>
+      <div class="content">
+        <p>Your inbox is getting full, would you like us to enable automatic archiving of old messages?</p>
+      </div>
+      <div class="actions">
+        <div class="ui red basic cancel inverted button">
+          <a href="https://arcanaut.github.io/Portfolio/" rel="noreferer" target="_blank">
+          No
+        </a>
+        </div>
+        <div class="ui green ok inverted button">
+          
+          Repository
+        </div>
+      </div>
     </div>
-    <button type="button" >
-      X
-    </button>
-    <img src={props.src} alt="current category" />
-    <div className="description">
-      <div className="ui header">We've auto-chosen a profile image for you.</div>
-      <p>We've grabbed the following image from the <a href="https://arcanaut.github.io/Portfolio/" rel="noreferrer" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
-    <button type="button">
-      <a >
-      Repository
-      </a>
-    </button>
-    <button type="button">
-    <a href="https://arcanaut.github.io/Portfolio/" rel="noreferrer" target="_blank">
-      Deployed Application
-      </a>
-    </button>
-  </div>
-</div>
-</div>
     );
 }
 
